38c754a724904cd678d5ca2d11119ee4727b2d8a
Jan Heiland <jnhlnd@gmail.com>
2024-03-15 17:39:09 +0100

    pd output mkdc and index into the index

--- mkdc.sh
+++ mkdc.sh
@@ -1,7 +1,7 @@
 FLNM=twolaypod
 INPFILE=${FLNM}.md
 
-pandoc ${FLNM}.md -o ${FLNM}.html \
+pandoc ${FLNM}.md -o index.html \
     --citeproc \
     --mathjax  \
     -t revealjs --slide-level=2 -s \
