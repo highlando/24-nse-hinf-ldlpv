46360bd9825caeddfd9caa1eb521e66e04407f12
Jan Heiland <jnhlnd@gmail.com>
2024-03-16 14:22:03 +0100

    use the standard reveal sources

--- mkdc.sh
+++ mkdc.sh
@@ -5,7 +5,7 @@ pandoc ${FLNM}.md -o index.html \
     --citeproc \
     --mathjax  \
     -t revealjs --slide-level=2 -s \
-    -V revealjs-url=/home/heiland/software/gits/reveal.js \
+    # -V revealjs-url=/home/heiland/software/gits/reveal.js \
     -V theme=solarized \
     -V NavigationMode=linear \
     -V viewDistance=15 -V width=1280 -V height=880 -V margin=0.05
